bf3dfefccc72dd803c477876acccd9ac30720043
mango-jam-0513 <mangojamstar0513@outlook.com>
2020-10-28 02:31:27 +0800

    new one

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,37 +1,4 @@
-//Change pos/background/padding/add shadow on nav when scroll event happens 
-$(function(){
-	var navbar = $('.navbar');
-	
-	$(window).scroll(function(){
-		if($(window).scrollTop() <= 40){
-			navbar.removeClass('navbar-scroll');
-            navbar.addClass('bg-transparent');
-		} else {
-			navbar.removeClass('bg-transparent');
-            navbar.addClass('navbar-scroll');
-		}
-	});
-});
 
-
-// Y axis scroll speed
-var velocity = 0.2;
-
-function update(){ 
-    var pos = $(window).scrollTop(); 
-    $('.fixed').css('backgroundPosition',   '50% ' + Math.round((0-pos) * velocity) + 'px');
-       
-};
-$(window).bind('scroll', update);
-
-// map
-function myMap() {
-	var mapProp= {
-	  center:new google.maps.LatLng(51.508742,-0.120850),
-	  zoom:5,
-	};
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	}
 
 //------------------ gallery------------------
 // Gallery image hover
@@ -121,3 +88,87 @@ $( ".img-wrapper" ).hover(
 	$("#overlay").fadeOut("normal");
   });
 //------------------ /gallery------------------
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+// select span
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+// ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
+
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+  // console.log(linksContainer.getBoundingClientRect());
+});
+
+// ********** fixed navbar ************
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  // setup back to top link
+
+  if (scrollHeight > 500) {
+    console.log("helo");
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // close
+    linksContainer.style.height = 0;
+  });
+});
+// calculate heights
